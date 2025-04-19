a02a4d1a46a380437676031d79dc243935285654
Ajay Rawat <104298002+AjayRawat0706@users.noreply.github.com>
2025-04-19 19:07:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const button = document.getElementById("btn");
+function solve(){
+	let val = document.getElementById("colorSelect").value;
+let colorselect=document.getElementById("colorSelect");
+	
+	   for (let i = 0; i < colorSelect.options.length; i++) {
+        if (colorSelect.options[i].value === val) {
+            colorSelect.remove(i); // Remove the option
+            break; // Exit loop after removing
+        }
+    }
+}
+
+button.addEventListener("click",solve);
